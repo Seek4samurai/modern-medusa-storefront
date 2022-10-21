10c2fec5699e7302af954727124531adf20fad58
Seek4samurai <grvrawat999@gmail.com>
2022-10-21 10:54:30 +0530

    added videos & footer styles

--- src/modules/layout/components/footer-cta/index.tsx
+++ src/modules/layout/components/footer-cta/index.tsx
@@ -3,22 +3,26 @@ import Image from "next/image"
 
 const FooterCTA = () => {
   return (
-    <div className="bg-fuchsia-100 w-full">
+    <div className="bg-fuchsia-100 w-full mt-[100px]">
       <div className="content-container flex flex-col-reverse gap-y-8 small:flex-row small:items-center justify-between py-16 relative">
         <div>
-          <h3 className="text-2xl-semi">Shop the latest styles</h3>
-          <div className="mt-6">
+          <h3 className="text-8xl font-bold">
+            <span className="bg-fuchsia-400">Shop</span> the latest
+            <br />
+            styles
+          </h3>
+          <div className="mt-6 animate-bounce">
             <UnderlineLink href="/store">Explore products</UnderlineLink>
           </div>
         </div>
 
-        <div className="relative w-full aspect-square small:w-[35%] small:aspect-[28/36]">
-          <Image
-            src="/cta_one_1.jpg"
-            alt=""
-            layout="fill"
-            objectFit="cover"
-            className="absolute inset-0 rounded-xl"
+        <div className="relative w-full h-[500px] flex items-center aspect-square small:w-[35%] small:aspect-[28/36]">
+          <video
+            autoPlay
+            loop
+            muted
+            src="/purplebg-girl-dancing.mp4"
+            className="absolute rounded-xl drop-shadow-2xl"
           />
         </div>
       </div>
--- src/modules/layout/components/footer-nav/index.tsx
+++ src/modules/layout/components/footer-nav/index.tsx
@@ -7,7 +7,7 @@ const FooterNav = () => {
   const { collections } = useCollections()
 
   return (
-    <div className="content-container flex flex-col gap-y-8 pt-16 pb-8">
+    <div className="content-container flex flex-col gap-y-8 pt-[200px] pb-8">
       <div className="flex flex-col gap-y-6 xsmall:flex-row items-start justify-between">
         <div>
           <Link href="/">
@@ -66,7 +66,7 @@ const FooterNav = () => {
         </div>
       </div>
       <div className="flex flex-col-reverse gap-y-4 justify-center xsmall:items-center xsmall:flex-row xsmall:items-end xsmall:justify-between">
-        <span className="text-small-regular text-gray-500">
+        <span className="text-small-regular text-fuchsia-800">
           © Copyright 2022{" "}
           <span className="font-bold">
             <a href="https://medusajs.com/" target={"_blank"} rel="noreferrer">
